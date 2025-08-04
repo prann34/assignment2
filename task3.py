e46885ad60f7219aa092fdc5d8b3e8b64df32fac
prann34 <prann3928@gmail.com>
2025-08-04 13:49:37 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,7 @@
+#Even Odd number
+a=input("Enter the number: ")
+a=int(a)
+if (a%2==0):
+    print(a," is an even number")
+else:
+    print(a," is an odd number")
---
+++ task4.py
@@ -0,0 +1,7 @@
+#Sum of numbers using for loop
+b=0
+a=range(1,51)
+for x in a:
+    b=b+x
+
+print("The sum is: ",b)
